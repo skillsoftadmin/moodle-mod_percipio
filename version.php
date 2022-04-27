061e8afb7ae977d53b3ab9dc9f17101c2b613c8f
skillsoftadmin <anand.mody@skillsoft.com>
2022-04-27 14:44:47 +0530

    version updated

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_percipio';
 $plugin->release = '1.0';
-$plugin->version = 2022022115;
+$plugin->version = 2022022116;
 $plugin->requires = 2018120307;
 $plugin->maturity = MATURITY_STABLE;
